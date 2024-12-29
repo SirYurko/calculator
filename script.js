f8d4ee88c10db6ba493ab3226968a548a8608f18
SirYurko <55402601+SirYurko@users.noreply.github.com>
2024-12-29 23:35:08 +0200

    add style, script

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let resultNumber = 0;
+
+
+document.getElementById('button7').addEventListener('click', logging)
+console.log()
+function logging () {
+    firstNumber = 
+    console.log()
+
+}
+
